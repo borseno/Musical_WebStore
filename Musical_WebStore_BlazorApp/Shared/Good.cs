2c989ffc84cbaf675ff18d8265e5e075eeda1172
AmiKirito <romankrol2000@gmail.com>
2019-10-16 19:43:03 +0300

    created Good, Instruments classes

---
+++ Musical_WebStore_BlazorApp/Shared/Good.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musical_WebStore_BlazorApp.Shared
+{
+    class Good
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+        
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+
+    }
+    class Instrument : Good
+    {
+
+    }
+
+}
