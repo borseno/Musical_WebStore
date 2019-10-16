609ef367dff7b18089e51e9f777e956885cbfeaa
borseno <borseno2@gmail.com>
2019-10-16 21:54:20 +0300

    added identitycontext

---
+++ Musical_WebStore_BlazorApp/Server/Data/MusicalShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Musical_WebStore_BlazorApp.Server.Data
+{
+    public class MusicalShopIdentityDbContext : IdentityDbContext
+    {
+        public MusicalShopIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- Musical_WebStore_BlazorApp/Server/Startup.cs
+++ Musical_WebStore_BlazorApp/Server/Startup.cs
@@ -1,25 +1,55 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Musical_WebStore_BlazorApp.Server.Data;
 using Newtonsoft.Json.Serialization;
+using System;
 using System.Linq;
 
 namespace Musical_WebStore_BlazorApp.Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+        {
+            string connstr = GetConnectionString(env);
+
+            services.AddDbContext<MusicalShopIdentityDbContext>(
+                options => options.UseSqlServer(connstr));
+
             services.AddMvc().AddNewtonsoftJson();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
                     new[] { "application/octet-stream" });
             });
+        }
+
+        private string GetConnectionString(IWebHostEnvironment env)
+        {
+            string dbName;
+
+            if (env.IsDevelopment())
+            {
+                dbName = "AuthenticationDB_Local";
+            }
+            else
+            {
+                dbName = "AuthenticationDB";
+            }
+
+            return Configuration.GetConnectionString(dbName);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
