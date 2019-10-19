1392ee1b75bfa39fd61c48b134cca0e3ec0fb693
borseno <borseno2@gmail.com>
2019-10-19 17:17:02 +0300

    added loginController

---
+++ Musical_WebStore_BlazorApp/Shared/LoginResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Musical_WebStore_BlazorApp.Shared
+{
+    public class LoginResult
+    {
+        public bool Successful { get; set; }
+        public string Error { get; set; }
+        public string Token { get; set; }
+    }
+}
