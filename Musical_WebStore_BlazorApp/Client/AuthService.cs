b9607d75f351e6070ebf499587da844b65e80bee
borseno <borseno2@gmail.com>
2019-10-21 18:14:16 +0300

    Encapsulated authentication (added AuthService)

---
+++ Musical_WebStore_BlazorApp/Client/AuthService.cs
@@ -0,0 +1,66 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Musical_WebStore_BlazorApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Musical_WebStore_BlazorApp.Client
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+        Task<RegisterResult> Register(RegisterModel registerModel);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthService(HttpClient httpClient,
+                           AuthenticationStateProvider authenticationStateProvider,
+                           ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authenticationStateProvider = authenticationStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<RegisterResult> Register(RegisterModel registerModel)
+        {
+            var result = await _httpClient.PostJsonAsync<RegisterResult>("api/accounts", registerModel);
+
+            return result;
+        }
+
+        public async Task<LoginResult> Login(LoginModel loginModel)
+        {
+            var result = await _httpClient.PostJsonAsync<LoginResult>("api/Login", loginModel);
+
+            if (result.Successful)
+            {
+                await _localStorage.SetItemAsync("authToken", result.Token);
+                ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(result.Token);
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public async Task Logout()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsLoggedOut();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
