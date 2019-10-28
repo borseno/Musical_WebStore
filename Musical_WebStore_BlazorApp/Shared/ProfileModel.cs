c6b161c214d545da53b6f145432a3ad268352835
Vlad Buryak <vladislavburyak00@gmail.com>
2019-10-28 21:30:32 +0200

    [add] profile page content, fields for profile model

---
+++ Musical_WebStore_BlazorApp/Shared/ProfileModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Musical_WebStore_BlazorApp.Shared
+{
+    public class ProfileModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
