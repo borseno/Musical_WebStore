1ed448dd1268cf31a3309a4ea9bfd7d17b359339
Vlad Buryak <vladislavburyak00@gmail.com>
2019-10-28 22:16:07 +0200

    [edit] UserService

--- Musical_WebStore_BlazorApp/Client/UserService.cs
+++ Musical_WebStore_BlazorApp/Client/UserService.cs
@@ -1,10 +1,23 @@
 using System;
-
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Musical_WebStore_BlazorApp.Shared;
 
 namespace Musical_WebStore_BlazorApp.Services
 {
     public class UserService
     {
+        private readonly HttpClient _httpClient;
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<EditUserResult> ChangeData(ProfileModel model)
+        {
+            var result = await _httpClient.PostJsonAsync<EditUserResult>("api/accounts", model);
+            return result;
+        }
         
     }
 }
