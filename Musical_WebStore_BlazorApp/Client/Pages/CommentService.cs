0e5782d752ea00d77f83985908365a2023e29603
Vlad Buryak <vladislavburyak00@gmail.com>
2019-11-09 16:29:34 +0200

    [add] comment service

---
+++ Musical_WebStore_BlazorApp/Client/Pages/CommentService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Musical_WebStore_BlazorApp.Shared;
+
+namespace Musical_WebStore_BlazorApp.Services
+{
+    public class CommentService
+    {
+        private readonly HttpClient _httpClient;
+        public CommentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<LeaveCommentResult> LeaveComment(CommentModel model)
+        {
+            var result = await _httpClient.PostJsonAsync<LeaveCommentResult>("api/comments/samplecomment", model);
+            return result;
+        }
+        
+    }
+}
