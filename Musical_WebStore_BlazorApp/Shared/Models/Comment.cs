2926d3ace9eb045a185b2ebd2ced32941dccbb85
Vlad Buryak <vladislavburyak00@gmail.com>
2019-11-09 14:38:23 +0200

    [add] comment entity

---
+++ Musical_WebStore_BlazorApp/Shared/Models/Comment.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musical_WebStore_BlazorApp.Shared
+{
+    public class Comment
+    {
+        public int Id {get;set;}
+        public string Text {get;set;}
+        public DateTime Date {get;set;}
+        public Good Instrument {get;set;}
+    }
+}
