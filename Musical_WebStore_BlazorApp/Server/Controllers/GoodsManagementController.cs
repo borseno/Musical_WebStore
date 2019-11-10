e688453f9e07848a4f4528f4a8ab41ab86fba613
borseno <borseno2@gmail.com>
2019-11-10 21:58:43 +0200

    added addGood blazor component TODO: implement uploading file

---
+++ Musical_WebStore_BlazorApp/Server/Controllers/GoodsManagementController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Musical_WebStore_BlazorApp.Server.Data;
+using Musical_WebStore_BlazorApp.Shared;
+
+namespace Musical_WebStore_BlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   // [Authorize("Admin")]
+    public class GoodsManagementController : ControllerBase
+    {
+        private readonly MusicalShopIdentityDbContext ctx;
+
+        public GoodsManagementController(MusicalShopIdentityDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> AddGood(AddGoodModel model)
+        {
+            ;
+            ;
+            ;
+
+            return Ok();
+        }
+    }
+}
---
+++ Musical_WebStore_BlazorApp/Shared/AddGoodModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Musical_WebStore_BlazorApp.Shared
+{
+    public class AddGoodModel
+    {
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageBytes { get; set; }
+        public string ImageType { get; set; }
+
+        public string TypeName { get; set; }
+    }
+}
