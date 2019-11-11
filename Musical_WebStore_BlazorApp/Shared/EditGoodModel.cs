cd3a810372204984009ee6ea419a4297a05e5f24
borseno <borseno2@gmail.com>
2019-11-11 18:26:54 +0200

    added editgoodmodel

---
+++ Musical_WebStore_BlazorApp/Shared/EditGoodModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musical_WebStore_BlazorApp.Shared
+{
+    public class EditGoodModel : AddGoodModel
+    {
+        public int Id { get; set; }
+    }
+}
