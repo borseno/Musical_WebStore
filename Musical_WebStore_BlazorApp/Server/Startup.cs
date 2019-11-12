9f860a5ae68930bce0a075a4865b3a4d819363d7
borseno <borseno2@gmail.com>
2019-11-12 16:54:23 +0200

    made helper methods extension methods and moved to startuphelper class

--- Musical_WebStore_BlazorApp/Server/Startup.cs
+++ Musical_WebStore_BlazorApp/Server/Startup.cs
@@ -1,20 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Musical_WebStore_BlazorApp.Server.Data;
-using Musical_WebStore_BlazorApp.Server.Data.Models;
 using Musical_WebStore_BlazorApp.Server.Helpers;
 using Musical_WebStore_BlazorApp.Server.Services;
-using System;
-using System.Linq;
-using System.Text;
 
 namespace Musical_WebStore_BlazorApp.Server
 {
@@ -31,10 +22,10 @@ namespace Musical_WebStore_BlazorApp.Server
 
         public void ConfigureServices(IServiceCollection services)
         {
-            AddDatabaseProvider(services);
-            AddIdentity(services);
-            AddCompression(services);
-            AddAuthentication(services);
+            services.AddDatabaseProvider( Configuration, Environment);
+            services.AddIdentity();
+            services.AddCompression();
+            services.AddAuthentication(Configuration);
 
             services.AddMvc().AddNewtonsoftJson();
             services.AddTransient<IEmailSender, MockeeMockersEmailSender>();
@@ -54,7 +45,7 @@ namespace Musical_WebStore_BlazorApp.Server
             
             if (Configuration.GetDatabaseType() == DatabaseType.InMemory)
             {
-                EnsureDatabaseCreated(app.ApplicationServices);
+                StartupHelper.EnsureDatabaseCreated<MusicalShopIdentityDbContext>(app.ApplicationServices);
             }
 
             app.UseStaticFiles();
@@ -72,81 +63,5 @@ namespace Musical_WebStore_BlazorApp.Server
             });
         }
 
-        private void EnsureDatabaseCreated(IServiceProvider serviceProvider)
-        {
-            using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
-            using var context = scope.ServiceProvider.GetService<MusicalShopIdentityDbContext>();
-            context.Database.EnsureCreated();
-        }
-
-        private void AddAuthentication(IServiceCollection services)
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                                .AddJwtBearer(options =>
-                                {
-                                    options.TokenValidationParameters = new TokenValidationParameters
-                                    {
-                                        ValidateIssuer = true,
-                                        ValidateAudience = true,
-                                        ValidateLifetime = true,
-                                        ValidateIssuerSigningKey = true,
-                                        ValidIssuer = Configuration["JwtIssuer"],
-                                        ValidAudience = Configuration["JwtAudience"],
-                                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSecurityKey"]))
-                                    };
-                                });
-        }
-
-        private static void AddCompression(IServiceCollection services)
-        {
-            services.AddResponseCompression(opts =>
-            {
-                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                    new[] { "application/octet-stream" });
-            });
-        }
-
-        private static void AddIdentity(IServiceCollection services)
-        {
-            services.AddDefaultIdentity<User>(options =>
-            {
-                options.User.RequireUniqueEmail = true;
-            }).AddEntityFrameworkStores<MusicalShopIdentityDbContext>()
-              .AddDefaultTokenProviders();
-        }
-
-        private void AddDatabaseProvider(IServiceCollection services)
-        {
-            var databaseType = Configuration.GetDatabaseType();
-
-            if (databaseType == DatabaseType.InMemory)
-            {
-                services.AddDbContext<MusicalShopIdentityDbContext>(
-                    options => options.UseInMemoryDatabase("random_name"));
-            }
-            if (databaseType == DatabaseType.SQLSERVER)
-            {                
-                var connStr = GetConnectionString(databaseType);
-
-                services.AddDbContext<MusicalShopIdentityDbContext>(
-                    options => options.UseSqlServer(connStr));
-            }
-        }
-
-        private string GetConnectionString(DatabaseType type)
-        {
-            string dbName;
-
-            if (Environment.IsDevelopment())
-            {
-                dbName = $"{(int)type}_AuthenticationDB_Local";
-            }
-            else
-            {
-                dbName = $"{(int)type}_AuthenticationDB";
-            }
-
-            return Configuration.GetConnectionString(dbName);
-        }
     }
 }
