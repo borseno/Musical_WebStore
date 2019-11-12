262874a1acf9521c96de4d66169d14b175f96e63
borseno <borseno2@gmail.com>
2019-11-12 15:19:14 +0200

    deleted lowercase folder

--- musical_webstore_blazorapp/Shared/DeleteStarModel.cs
+++
@@ -1,7 +0,0 @@
-namespace Musical_WebStore_BlazorApp.Shared
-{
-    public class DeleteStarModel
-    {
-        public int StarId {get;set;}
-    }
-}
--- musical_webstore_blazorapp/Shared/DeleteStarResult.cs
+++
@@ -1,8 +0,0 @@
-namespace Musical_WebStore_BlazorApp.Shared
-{
-    public class DeleteStarResult
-    {
-        public bool Successful { get; set; }
-        public string Error { get; set; }
-    }
-}
