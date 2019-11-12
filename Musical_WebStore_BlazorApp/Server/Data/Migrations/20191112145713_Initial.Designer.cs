5c18554b90830d51a8b7e1223bb92064bc168e59
borseno <borseno2@gmail.com>
2019-11-12 17:22:32 +0200

    added sql server support (Update-database command is required)

--- Musical_WebStore_BlazorApp/Server/Data/Migrations/20191112145713_Initial.Designer.cs
+++ Musical_WebStore_BlazorApp/Server/Data/Migrations/20191112145713_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Musical_WebStore_BlazorApp.Server.Data;
 
 namespace Musical_WebStore_BlazorApp.Server.Data.Migrations
 {
     [DbContext(typeof(MusicalShopIdentityDbContext))]
-    partial class MusicalShopIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191112145713_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
