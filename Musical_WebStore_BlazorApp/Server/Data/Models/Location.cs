c8fdc4d5e5eee95ed097ea255358d06eca1b56af
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-26 20:32:07 +0200

    [add] location entity

---
+++ Musical_WebStore_BlazorApp/Server/Data/Models/Location.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Musical_WebStore_BlazorApp.Server.Data.Models
+{
+    public class Location
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Address {get;set;}
+        public string Phone {get;set;}   
+    }
+}
