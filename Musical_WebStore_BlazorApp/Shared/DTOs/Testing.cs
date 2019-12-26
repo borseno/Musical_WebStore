8d83f508f5b0919a9bc7d61d2ede0baa5f003ecf
buckleQ <buckleqq2@gmail.com>
2019-12-26 19:48:21 +0200

    registered all services, added all nuget references and added links to pages

---
+++ Musical_WebStore_BlazorApp/Shared/DTOs/Testing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Musical_WebStore_BlazorApp.Shared.DTOs
+{
+    public class TestingDTO
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Comment { get; set; }
+
+        public bool IsConfirmed { get; set; }
+        
+        public string UserId { get; set; }
+
+        public UserLimited User { get; set; }
+
+        public Instrument Instrument { get; set; }
+    }
+}
