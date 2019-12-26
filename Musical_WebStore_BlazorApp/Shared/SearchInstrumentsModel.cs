5748430036a16dfd85fc00209ba9b567ea4bf872
Vlad Buryak <vladislavburyak00@gmail.com>
2019-12-27 01:09:34 +0200

    [add] model for search

---
+++ Musical_WebStore_BlazorApp/Shared/SearchInstrumentsModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Musical_WebStore_BlazorApp.Shared.DTOs
+{
+    public class SearchInstrumentsModel
+    {
+        public string Input {get;set;}
+        public string MinPrice {get;set;}
+        public string MaxPrice {get;set;}
+
+    }
+}
